50443e52e95964f7f045018f5249cb24b82b9b6f
itschool <itschool@tou.edu.kz>
2024-10-13 22:47:46 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// let a = [1, "Hello", 3, 4];
+// console.log(a);
+// console.table(a);
+// a[3] = 44;
+// console.log(a[3]);
+// a[4] = "New value";
+// a[5] = 2024;
+// console.table(a);
+// console.log(a.length);
+
+let b = [
+    1, 2, [
+        "hello", "world", [
+            "a", "b", "c"
+        ]
+    ], 3, 4
+];
+
+console.log(b[2][2][1]);
